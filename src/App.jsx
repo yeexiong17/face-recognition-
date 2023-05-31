5102e08f586989479dd734da7fe75b263e4b00c2
yeexiong17 <wyeexiong17@gmail.com>
2023-05-31 12:58:58 +0800

    Done frontend for this website

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import ParticlesBg from 'particles-bg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank'
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+
+      <ParticlesBg className="paticles" color='#ffffff' type="cobweb" bg={true} />
+
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* 
+      
+      <FaceRecognition /> */}
+
+    </div>
+  )
+}
+
+export default App
