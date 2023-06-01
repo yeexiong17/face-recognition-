e4ffee34aeb027df510772f43573466e2dafd747
yeexiong17 <wyeexiong17@gmail.com>
2023-06-01 13:39:56 +0800

    Face recognition api has done and is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,109 @@
+import { Component } from 'react';
 import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank'
-
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css';
 
-function App() {
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const IMAGE_URL = imageUrl;
 
-  return (
-    <div className='App'>
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": "yeexiong",
+      "app_id": "face-detection-app"
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
 
-      <ParticlesBg className="paticles" color='#ffffff' type="cobweb" bg={true} />
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + 'bacc862ed3ee4a3299551f64f759aa91'
+    },
+    body: raw
+  };
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* 
-      
-      <FaceRecognition /> */}
+  return requestOptions;
+}
 
-    </div>
-  )
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    console.log(this.state.imageUrl);
+
+    // app.models.predict(
+    //   "36742fb53a134dedad94e019c1786253",
+    //   "https://samples.clarifai.com/face-det.jpg")
+
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, returnClarifaiRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className='App' >
+
+        <ParticlesBg className="paticles" color='#ffffff' type="cobweb" bg={true} />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -8,8 +8,8 @@ const Logo = () => {
     return (
         <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }}>
-                <div className='Tilt-inner pa3'>
-                    <img style={{ paddingTop: '5px' }} src={brain} alt="logo" />
+                <div className='Tilt-inner pa3 h-100 flex justify-center items-center'>
+                    <img className='h-75' src={brain} alt="logo" />
                 </div>
             </Tilt>
         </div>
